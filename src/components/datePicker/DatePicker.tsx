a11e705d33df828c91d71395b2677cc17c130570
kihyeoon <bangion93@gmail.com>
2024-01-21 02:49:49 +0900

    ✨ 캘린더 컴포넌트에 년, 월 셀렉트 박스 추가

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import * as React from "react";
 import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import { ko } from "date-fns/locale";
+import { CalendarIcon } from "lucide-react";
+import * as React from "react";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
@@ -12,6 +12,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+
+import { cn } from "@/lib/utils";
 
 export function DatePickerDemo() {
   const [date, setDate] = React.useState<Date>();
@@ -22,20 +24,27 @@ export function DatePickerDemo() {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
+            "w-[240px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          {date ? (
+            format(date, "PPP", { locale: ko })
+          ) : (
+            <span>날짜를 선택하세요</span>
+          )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent align="start" className=" w-auto p-0">
         <Calendar
           mode="single"
+          captionLayout="dropdown-buttons"
           selected={date}
           onSelect={setDate}
-          initialFocus
+          fromYear={1900}
+          toYear={new Date().getFullYear()}
+          locale={ko}
         />
       </PopoverContent>
     </Popover>
